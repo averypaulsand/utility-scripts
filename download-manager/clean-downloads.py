818b92f66d15c3e085045024517b92ee65072f1f
averypaulsand <37809935+averypaulsand@users.noreply.github.com>
2020-12-10 09:51:34 -0800

    Update clean-downloads.py

--- download-manager/clean-downloads.py
+++ download-manager/clean-downloads.py
@@ -5,18 +5,21 @@ import sys
 # global path variables
 downloads_dir = str(Path('/Users/ave/Downloads'))
 music_dir = str(Path('/Users/ave/Music/Library'))
-movies_dir = str(Path('/Users/ave/Music/Movies'))
+movies_dir = str(Path('/Users/ave/Movies'))
 pictures_dir = str(Path('/Users/ave/Pictures/'))
-readings_dir = str(Path('/Users/ave/Readings/'))
+readings_dir = str(Path('/Users/ave/Documents/Readings/PDFS'))
+downloads_path = Path('/Users/ave/Downloads').glob('**/*')
 downloads_zips = Path('/Users/ave/Downloads').glob('**/*.zip')
-#downloads_mp3s = Path('/Users/ave/Downloads').glob('**/*.mp3')
-#downloads_mp4s = Path('/Users/ave/Downloads').glob('**/*.mp4')
-#downloads_pdfs = Path('/Users/ave/Downloads').glob('**/*.pdf')
+downloads_mp4s = Path('/Users/ave/Downloads').glob('**/*.mp4')
+downloads_pdfs = Path('/Users/ave/Downloads').glob('**/*.pdf')
+downloads_dmgs = Path('/Users/ave/Downloads').glob('**/*.dmg')
 
 
 def clean_downloads():
     unzip()
-    # remove_dmgs()
+    move_pdfs()
+    move_mp4s()
+    trash_dmgs()
 
 
 # unzips any zipped files and then moves the unzipped folder to trash
@@ -29,7 +32,7 @@ def unzip():
         subprocess.run(["unzip", str(filepath), "-d", str(dst_dir)])
         move_music(dst_dir)
         subprocess.run(["trash", str(filepath)])
-        #subprocess.run(["trash", "/Users/ave/Downloads/__MACOSX"])
+        # subprocess.run(["trash", "/Users/ave/Downloads/__MACOSX"])
 
 
 def move_music(music_folder):
@@ -37,6 +40,21 @@ def move_music(music_folder):
     subprocess.run(["trash", str(music_folder)])
 
 
-# calls function when .py script is executed
+def move_pdfs():
+    for filepath in downloads_pdfs:
+        subprocess.run(["mv", str(filepath), readings_dir])
+
+
+def move_mp4s():
+    for filepath in downloads_mp4s:
+        subprocess.run(["mv", str(filepath), movies_dir])
+
+
+def trash_dmgs():
+    for filepath in downloads_dmgs:
+        subprocess.run(["trash", str(filepath)])
+
+
+# calls function only when .py script is executed
 if __name__ == '__main__':
     clean_downloads()
